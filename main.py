ee8b46fab7cb2dda3ea59ce2548027dc2a07fb31
Reading, Delicacy & Tea <47962072+VanillaChocoCake@users.noreply.github.com>
2023-07-17 13:09:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import re
 import pyperclip
-import time
 
-pre_content = content = pyperclip.paste()
-print("Will remove all \\n in clipboard")
-while True:
-    content = pyperclip.paste()
-    if pre_content != content:
-        pre_content = content
-        changed = re.sub(r"\s{2,}", " ", pre_content)
-        pyperclip.copy(changed)
-        time.sleep(0.1)
+
+# 实时检测粘贴板数据
+def watch_clipboard():
+    while True:
+        clipboard_text = pyperclip.paste()  # 获取粘贴板中的文本
+        processed_text = re.sub(r'[\r\n]+', ' ', clipboard_text)  # 将换行符替换为空格
+
+        # 替换粘贴板中的文本
+        if processed_text != clipboard_text:
+            pyperclip.copy(processed_text)
+
+        # 继续监听粘贴板
+        pyperclip.waitForNewPaste()
+
+
+# 运行程序
+if __name__ == "__main__":
+    print("Will remove all \\n in clipboard")
+    watch_clipboard()
